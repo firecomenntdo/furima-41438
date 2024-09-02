51aa200fbe7f5ee3650bfe8d6f4bbe4bd0b5f54d
firecomenntdo <shikaneko6@gmail.com>
2024-09-02 18:45:57 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/ }, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, format: { with: /\A[a-z0-9]+\z/ }, length: { minimum: 6 }
-  validates :lastname, presence: true
-  validates :firstname, presence: true
+  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
+                                    length: { minimum: 6 }
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth, presence: true
