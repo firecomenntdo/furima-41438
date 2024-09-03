b2cd76e8e90a78d934331bbd150a456fe34f1715
firecomenntdo <shikaneko6@gmail.com>
2024-09-03 12:41:33 +0900

    user単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/ }, length: { minimum: 6 }
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は英字と数字を含めてください' },
+                       length: { minimum: 6 }
   validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
                                     length: { minimum: 6 }
   validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :birth, presence: true
 end
