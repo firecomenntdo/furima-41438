196891f8a6d7212ffd117808469c73a9520a6fc5
firecomenntdo <shikaneko6@gmail.com>
2024-09-09 14:55:02 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,11 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    # item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit, status: :unprocessable_entity
+      puts @item.errors.full_messages
     end
   end
 
